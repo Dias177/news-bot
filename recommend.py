5d41005fa9ef393a9d31111ea010139fa3399158
Dias177 <zhassanbay@gmail.com>
2020-04-03 21:51:55 +0600

    refactor: handle db-related errors

--- recommend.py
+++ recommend.py
@@ -17,25 +17,28 @@ from news import News
 
 def recommend(user_id, news, num_news):
     data = get_data(user_id)
-    corpus, y = [], []
-    for row in data:
-        news_title = row[1]
-        is_interested = row[-2]
-        preprocessed_news_title = preprocess_text(news_title)
-        corpus.append(preprocessed_news_title)
-        y.append(is_interested)
-    
-    for n in news:
-        preprocessed_news_title = preprocess_text(n)
-        corpus.append(preprocessed_news_title)
-    
-    cv = CountVectorizer(max_features = 1000)
-    X = cv.fit_transform(corpus).toarray()
-    classifier = GaussianNB()
-    classifier.fit(X[:-num_news], y)
-    y_pred = classifier.predict(X[-num_news:])
-    
-    return y_pred
+    if data:
+        corpus, y = [], []
+        for row in data:
+            news_title = row[1]
+            is_interested = row[-2]
+            preprocessed_news_title = preprocess_text(news_title)
+            corpus.append(preprocessed_news_title)
+            y.append(is_interested)
+        
+        for n in news:
+            preprocessed_news_title = preprocess_text(n)
+            corpus.append(preprocessed_news_title)
+        
+        cv = CountVectorizer(max_features = 1000)
+        X = cv.fit_transform(corpus).toarray()
+        classifier = GaussianNB()
+        classifier.fit(X[:-num_news], y)
+        y_pred = classifier.predict(X[-num_news:])
+        
+        return y_pred
+    else:
+        return None
 
 def preprocess_text(text):
     mystem = Mystem() 
@@ -51,16 +54,19 @@ def preprocess_text(text):
     return text
 
 def get_data(user_id):
-    mydb = mysql.connector.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            passwd=DB_PASSWORD,
-            database=DB_NAME
-    )
-    mycursor = mydb.cursor()
+    try:
+        mydb = mysql.connector.connect(
+                host=DB_HOST,
+                user=DB_USER,
+                passwd=DB_PASSWORD,
+                database=DB_NAME
+        )
+        mycursor = mydb.cursor()
 
-    mycursor.execute(f'SELECT * FROM news WHERE user_user_id={user_id}')
-    res = mycursor.fetchall()
-    mycursor.close()
-    mydb.close()
-    return res
+        mycursor.execute(f'SELECT * FROM news WHERE user_user_id={user_id}')
+        res = mycursor.fetchall()
+        mycursor.close()
+        mydb.close()
+        return res
+    except:
+        return None
